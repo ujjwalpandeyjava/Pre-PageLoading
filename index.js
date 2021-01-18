f3e75720dc4ebd430ee65e3c19543b444df3611e
Ujjwal Pandey <ujjwalpandey.aps@gmail.com>
2021-01-18 20:20:11 +0530

    All saved

---
+++ index.js
@@ -0,0 +1,9 @@
+//For animation don't change
+const tl = gsap.timeline({ defaults: { ease: 'power1.out' } });
+
+
+tl.to('.text', { y: '0%', duration: 1, stagger: 0.25 })
+tl.to('.slider', { y: '-100%', duration: 1.2, delay: 0.25 })
+tl.to('.intro', { y: '-100%', duration: 1 }, "-=1")
+tl.fromTo('nav',{opacity:0},{opacity:1,duration:1})
+tl.fromTo('.big-text',{opacity:0},{opacity:1,duration:1},"-=1")
